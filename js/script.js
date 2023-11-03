61294650d8f513e69f813adcb3dd4ddbd923d3ec
G0blinC0de <brewbrotherred@gmail.com>
2023-11-03 15:22:06 -0700

    add initial javascript functionality

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('select-form');
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+        })
+
+});
