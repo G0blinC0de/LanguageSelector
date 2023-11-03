c15a80f64e64138b13860916714b74b65bcd8533
G0blinC0de <brewbrotherred@gmail.com>
2023-11-03 15:49:34 -0700

    added js branching for the first y/n boolean

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const selectForm = document.getElementById('select-form');
+    const radioForm = document.getElementById('radio-form');
+    const ytho = document.getElementById('ytho');
+
+    radioForm.classList.add('hidden');
 
         selectForm.addEventListener('submit', function(event) {
             event.preventDefault();
-            
+            const selectedOption = interest.options[interest.selectedIndex].value;
+                if (selectedOption === 'Yes') {
+                    radioForm.classList.remove('hidden');
+                    ytho.classList.add('hidden');
+                } else {
+                    ytho.classList.remove('hidden');
+                    radioForm.classList.add('hidden');
+                }
+
+    
         })
 
 });
