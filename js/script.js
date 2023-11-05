23ec31e8f85e802428a27d830e2849f9eb3be820
G0blinC0de <brewbrotherred@gmail.com>
2023-11-04 20:39:31 -0700

    add third question to HTML

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const selectForm = document.getElementById('select-form');
     const radioForm = document.getElementById('radio-form');
     const ytho = document.getElementById('ytho');
+    const reveal = document.getElementById('reveal');
 
     radioForm.classList.add('hidden');
+    reveal.classList.add('hidden');
 
         selectForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -15,7 +17,18 @@ document.addEventListener('DOMContentLoaded', function() {
                     ytho.classList.remove('hidden');
                     radioForm.classList.add('hidden');
                 }
+        radioForm.addEventListener('submit', function(event){
+            event.preventDefault();
+            const selectedPet = document.querySelector('input[name="pet"]:checked').value;
+            if (selectedPet === 'cat') {
+                reveal.classList.remove('hidden');
 
+            } else if (selectedPet === 'dog') {
+                ytho.classList.remove('hidden');
+                radioForm.classList.add('hidden');
+                reveal.classList.add('hidden');
+            }
+        })
     
         })
 
